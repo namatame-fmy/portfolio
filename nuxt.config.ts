c873f0e8a5ff0c139b0847ddd6b0a633da7efcf5
namatame-fmy <mariypuuu0519@gmail.com>
2025-03-18 16:49:58 +0900

    microcms関係の修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindTypography from "@tailwindcss/typography";
+import tailwindTypography from '@tailwindcss/typography'
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
@@ -8,26 +8,26 @@ export default defineNuxtConfig({
     'nuxt-microcms-module',
     '@nuxtjs/color-mode',
   ],
-    microCMS: {
-      serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
-      apiKey: process.env.MICROCMS_API_KEY,
-      target: 'all'
+  microcms: {
+    serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+    apiKey: process.env.MICROCMS_API_KEY,
+    target: 'all',
+  },
+  googleFonts: {
+    families: {
+      // 英語用セリフフォント
+      'Noto+Serif': [300, 400, 700],
+      // 日本語用サンセリフフォント
+      'Noto+Sans+JP': [300, 400, 700],
+      // MV用の特別なフォント
+      'Playfair+Display': [400, 700],
     },
-    googleFonts: {
-      families: {
-        // 英語用セリフフォント
-        'Noto+Serif': [300, 400, 700],
-        // 日本語用サンセリフフォント
-        'Noto+Sans+JP': [300, 400, 700],
-        // MV用の特別なフォント
-        'Playfair+Display': [400, 700]
-      },
-      display: 'swap',
-      preload: true,
-    },
+    display: 'swap',
+    preload: true,
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   colorMode: {
     classSuffix: '',
-  }
+  },
 })
